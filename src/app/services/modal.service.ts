82c567becf5b176f376977626ae1a07afb7272ea
Shazeen Fabius <shazzy.sterlin@gmail.com>
2023-02-11 00:06:08 -0800

    able to open and close modal

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+// @Injectable()
+export class ModalService {
+  private visible = false;
+  constructor() {}
+
+  isModalVisible() {
+    return this.visible;
+  }
+
+  toggleModal() {
+    this.visible = !this.visible;
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purgecss
+  content: ["./src/**/*.{html, ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
